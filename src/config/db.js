59c2c3d74239d76b72aedb83db1bb3ce95b47142
sandroluisj <sandroluisjr06@gmail.com>
2024-10-14 17:16:30 -0300

    sandro

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     mongoose 
-        .connect('mongodb://localhost:27017/mecanica')
+        .connect('`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`')
         .then(()=> console.log('connected to mongoDB'))
         .catch((error) => console.log(error))
 }
