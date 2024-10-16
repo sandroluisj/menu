23e99639b9977c2c212e96cc75c6782e40cfa191
sandro luis de almeida junior <sandroluisjr06@gmail.com>
2024-10-16 17:46:24 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     mongoose 
-        .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`)
+        .connect(`${process.env.URI}`)
         .then(()=> console.log('connected to mongoDB'))
         .catch((error) => console.log(error))
 }
